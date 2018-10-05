1a4d90ec897e7e79b49b92d521b70566cd391242
Anandha Sudhan <rasudhan@gmail.com>
2018-10-05 02:28:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis=document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML,10)+n).toString();
+  }
+
+}
+
+function deepestChild() {
+  const nodes=document.querySelectorAll('#grand-node div')
+
+
+  return nodes[nodes.length-1];
+
+}
