152d75649462feceb8bcba70aa9485a0c084245a
Anandha Sudhan <rasudhan@gmail.com>
2018-10-05 02:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
+function increaseRankBy(n) {
+  const lis=document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML,10)+n).toString();
+  }
+
+}
+
 function deepestChild() {
+  const lis=document.querySelector('div#grand-node')
+
+  while(lis) {
+    
+  }
 
 }
